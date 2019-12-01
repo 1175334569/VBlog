12f55355851db8ae2ad98c5518df936c57c9bc54
lyx <1175334569@qq.com>
2019-12-01 22:44:59 +0800

    2019.12.1

--- blogserver/src/main/java/org/sang/BlogserverApplication.java
+++ blogserver/src/main/java/org/sang/BlogserverApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling//开启定时任务支持
 public class BlogserverApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BlogserverApplication.class, args);
     }
